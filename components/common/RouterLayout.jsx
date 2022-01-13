d578b4a73e5416f088e73e0b49f643632449e534
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2022-01-13 17:57:08 +0100

    added title document; adding techs logo in progress; homepage what i do section in progress;

--- components/common/RouterLayout.jsx
+++ components/common/RouterLayout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Container } from "react-bootstrap"
 import Footer from "./footer/Footer";
 import NavBar from "./navbar/NavBar";
@@ -5,6 +6,10 @@ import NavBar from "./navbar/NavBar";
 const RouterLayout = ({children}) => {
 
     return (<>
+        <Head>
+            <title>Stefano Satta - JS Dev</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
         <NavBar/>
             <main>
                 <Container fluid className="p-0 bg-dark">
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import Img from 'next/image'
 import Bubble from "../components/common/bubble/Bubble";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,6 +6,10 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(fas, fab)
+
+import rxjsLogo from '../public/rxjs-logo.svg'
+import webpackLogo from '../public/webpack-logo.png'
+import Image from "next/image";
 
 
 const Home = () => {
@@ -20,7 +24,7 @@ const Home = () => {
           <div>
             <p className="fs-3 mb-0 text-light-grey">
               <span className="pe-3">Hi, there!</span> 
-              <Img src="https://twemoji.maxcdn.com/2/72x72/1f44b.png" width={40} height={40} className=""/> 
+              <Img src="https://twemoji.maxcdn.com/2/72x72/1f44b.png" width={40} height={40} id="hand-greeting"/> 
             </p>
             <h1 className="text-uppercase text-family-montserrat" style={{fontSize: '5.375rem',}}>Stefano<br/> Satta</h1>
             <div className="bg-primary w-50 p-1"></div>
@@ -38,6 +42,36 @@ const Home = () => {
           <Bubble style={{bottom: '20%', right: '25%'}}>
             <FontAwesomeIcon icon={['fab', 'angular']} size={'3x'} className="bg-icon-angular"/>
           </Bubble>
+        </Col>
+      </Row>
+
+
+      <Row className="mt-5 py-5">
+        <h2 className="fs-1 text-primary mb-4">What I <span className="text-primary">Do</span></h2>
+        <Col xs={12} md={6} className="d-flex align-items-center position-relative">
+          <div>
+            <p>For over eight years I had many opportunities to work in different projects, private and public administration. Having an experience of developing web applications and websites in different web technologies. <br /> My first passion is the frontend side but i have developed also in Nodejs/Express and MongoDB.</p>
+            <p>I currently work at Accenture, Cagliari, IT.</p>
+          </div>
+        </Col>
+        <Col xs={12} md={6} className="d-flex align-items-center justify-content-end position-relative">
+          <div className="">
+            <div className="mb-3 d-flex">
+              <FontAwesomeIcon icon={['fab', 'html5']} size={'3x'} className="text-less-grey me-4"/>
+              <FontAwesomeIcon icon={['fab', 'css3-alt']} size={'3x'} className="text-less-grey me-4"/>
+              <FontAwesomeIcon icon={['fab', 'sass']} size={'3x'} className="text-less-grey me-4"/>
+              <FontAwesomeIcon icon={['fab', 'js']} size={'3x'} className="bg-ictext-less-grey me-4"/>
+              <FontAwesomeIcon icon={['fab', 'angular']} size={'3x'} className="bgtext-less-grey me-4"/>
+              <Image src={rxjsLogo} width={50} height={50}/>
+            </div>
+            <div className="d-flex">
+              <FontAwesomeIcon icon={['fab', 'react']} size={'3x'} className="text-less-grey me-4"/>
+              <FontAwesomeIcon icon={['fab', 'node']} size={'3x'} className="text-less-grey me-4"/>
+              <FontAwesomeIcon icon={['fab', 'node']} size={'3x'} className="text-less-grey me-4"/>
+              <Image src={webpackLogo} width={50} height={50} className="text-less-grey"/>
+              <FontAwesomeIcon icon={['fab', 'git']} size={'3x'} className="ms-4 text-less-grey"/>
+            </div>
+          </div>
         </Col>
       </Row>
     </Container>
