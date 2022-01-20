52203ebf82bd0db52b4dd221a285d61b7c1a033b
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2022-01-20 15:17:28 +0100

    added initialLoader component and implemented to router layout

--- components/common/RouterLayout.jsx
+++ components/common/RouterLayout.jsx
@@ -3,30 +3,17 @@ import { Container } from "react-bootstrap"
 import Footer from "./footer/Footer";
 import NavBar from "./navbar/NavBar";
 import React, {useEffect, useState} from "react";
-import Image from "next/image";
-
-
-const Loader = () => (<>
-    <div>
-        <p className="fs-3 mb-0 text-light-grey">
-            <span className="pe-3">Hi, there!</span>
-            <Image src="https://twemoji.maxcdn.com/2/72x72/1f44b.png" width={40} height={40} id="hand-greeting"/>
-        </p>
-        <h1 className="text-uppercase text-family-montserrat fs-name" >Stefano<br/> Satta</h1>
-        <div className="bg-primary w-50 p-1"/>
-    </div>
-    <p className="text-primary">loader</p>
-</>)
+import InitialLoader from "./loader/InitialLoader";
 
 
 
 const RouterLayout = ({children}) => {
-    const [loader, setLoader] = useState(false);
+    const [loader, setLoader] = useState(true);
 
     useEffect(() => {
         setTimeout(() => {
-            setLoader(false)
-        }, 3000)
+            setLoader(false);
+        }, 2500)
     },[])
 
 
@@ -36,7 +23,9 @@ const RouterLayout = ({children}) => {
             <title>Stefano Satta - JS Dev</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
-        { loader && <Loader/> }
+        { loader && <InitialLoader/>}
+
+
         { !loader && (<>
             <NavBar/>
             <main>
@@ -45,8 +34,8 @@ const RouterLayout = ({children}) => {
                 </Container>
             </main>
             <Footer/>
-        </>)}
-
+        </>)
+        }
 
     </>)
 }
---
+++ components/common/loader/InitialLoader.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React, {useEffect, useRef} from "react";
+
+const InitialLoader = () => {
+    const counterText = useRef();
+
+    useEffect(() => {
+        animateValue(counterText.current, 0, 100, 2500)
+    }, [])
+
+    const animateValue = (element, start, end, duration) => {
+        let startTimestamp = null;
+
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            element.innerHTML = Math.floor(progress * (end - start) + start);
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+
+        window.requestAnimationFrame(step);
+    }
+
+
+    return (<>
+        <div className="w-100 vh-100 bg-dark d-flex justify-content-center align-items-center">
+            <div className="position-absolute text-center w-100 vh-100 d-flex justify-content-center align-items-center"
+                 style={{zIndex: 1, fontSize: '100vh', opacity: .2}}
+                 ref={counterText}>
+            </div>
+            <div className="position-relative" style={{zIndex: 10}}>
+                <p className="mb-0 text-white" style={{fontSize: '13.0952380952vw'}}>
+                    <span className="pe-3">Hi, there!</span>
+                    <Image src="https://twemoji.maxcdn.com/2/72x72/1f44b.png" width={40} height={40} id="hand-greeting"/>
+                </p>
+            </div>
+        </div>
+    </>)
+}
+
+export default InitialLoader;
