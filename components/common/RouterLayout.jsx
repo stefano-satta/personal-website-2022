6ab65e5bb4f45022d0468431f39948c31dcad384
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2022-08-18 12:13:54 +0200

    fix google font

--- components/common/RouterLayout.jsx
+++ components/common/RouterLayout.jsx
@@ -25,6 +25,11 @@ const RouterLayout = ({children}) => {
         <Head>
             <title>Stefano Satta - JS Dev</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&display=optional"
+                rel="stylesheet"
+            />
+            
         </Head>
 
         { 
