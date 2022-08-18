ee76af3325405c19ff5bf30b1ee5832f992873eb
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2022-08-18 12:38:38 +0200

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import RouterLayout from '../components/common/RouterLayout';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'animate.css';
-import '../styles/global.css';
+import './../styles/global.css';
 
 
 function MyApp({ Component, pageProps }) {
