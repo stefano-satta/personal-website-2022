8165b47d93938d6ae93d784ed3800b610ea665ac
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2022-08-18 15:05:13 +0200

    fix

--- components/common/footer/Footer.jsx
+++ components/common/footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     
     return (
-        <footer className="footer text-center py-4 mt-auto bg-dark">
+        <footer className="footer text-center py-4 mt-auto bg-dark text-white">
             <p className="mb-0">&copy; 2022 Stefano Satta</p>
         </footer>
     )
