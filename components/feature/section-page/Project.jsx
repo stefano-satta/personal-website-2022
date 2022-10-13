355931a5b220307a047f0c844b76b886db88cb94
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2022-10-13 12:27:34 +0200

    fix project and contact section: height single project box adjustment

--- components/feature/section-page/Project.jsx
+++ components/feature/section-page/Project.jsx
@@ -1,4 +1,4 @@
-import {Row} from 'react-bootstrap'
+import {Row, Col} from 'react-bootstrap'
 import ProjectCard from '../../common/project-card/ProjectCard'
 import { PROJECTS } from "../../../utility/project-list";
 
@@ -7,15 +7,17 @@ const Project = () => {
 
     return (
         <Row className="mt-5" id="project">
-            <h2 className="text-primary mb-4 fw-bold fs-title-section">Projects</h2>
-            <p className="fs-4">A small list of projects chosen by me.</p>
-            <div className="mt-3 d-flex flex-wrap justify-content-between">
-            {
-                PROJECTS.map( (project, index) => (
-                    <ProjectCard project={project} key={`${project.name}-${index}`}/>
-                )) 
-            }
-            </div>
+            <Col>
+                <h2 className="text-primary mb-4 fw-bold fs-title-section">Projects</h2>
+                <p className="fs-4">A small list of projects chosen by me.</p>
+                <div className="mt-3 d-flex flex-wrap justify-content-between">
+                    {
+                        PROJECTS.map( (project, index) => (
+                            <ProjectCard project={project} key={`${project.name}-${index}`}/>
+                        )) 
+                    }
+                </div>
+            </Col>
         </Row>
     )
 }
