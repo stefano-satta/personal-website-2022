3c4be761fc39c0ca694317f18133ffeaef13bc87
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2023-01-10 17:37:38 +0100

    added useCurrenteDate and added current year to footer

--- components/common/footer/Footer.jsx
+++ components/common/footer/Footer.jsx
@@ -1,8 +1,11 @@
+import useCurrentDate from "../../../hooks/useCurrentDate";
+
 const Footer = () => {
+    const {getCurrentYear} = useCurrentDate();
     
     return (
         <footer className="footer text-center py-4 mt-auto bg-dark text-white">
-            <p className="mb-0">&copy; 2022 Stefano Satta</p>
+            <p className="mb-0">&copy; {getCurrentYear()} Stefano Satta</p>
         </footer>
     )
 }
